a2d5d2dc2f40a6b87c443fb2369b9d1796295180
zhangzhaoyuan <kz_zzy@sina.cn>
2018-06-20 22:02:31 +0800

    架构设计

---
+++ guaiguo-dps-web/src/main/java/top/guaiguo/springdps/SpringDpsApplication.java
@@ -0,0 +1,12 @@
+package top.guaiguo.springdps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDpsApplication.class, args);
+	}
+}
