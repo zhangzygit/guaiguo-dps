437946fb4c6e5f457bf8e010ee79765243f1bf9f
CHUNBO\zhangzhaoyuan <zhangzhaoyuan@chunbo.com>
2018-06-27 15:30:44 +0800

    success

---
+++ guaiguo-dps-model/src/main/java/top/guaiguo/springdps/GuaiguoDpsModelApplication.java
@@ -0,0 +1,11 @@
+package top.guaiguo.springdps;
+
+import org.springframework.boot.SpringApplication;
+
+//@SpringBootApplication
+public class GuaiguoDpsModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuaiguoDpsModelApplication.class, args);
+    }
+}
---
+++ guaiguo-dps-service/src/main/java/top/guaiguo/springdps/GuaiguoDpsServiceApplication.java
@@ -0,0 +1,11 @@
+package top.guaiguo.springdps;
+
+import org.springframework.boot.SpringApplication;
+
+//@SpringBootApplication
+public class GuaiguoDpsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuaiguoDpsServiceApplication.class, args);
+    }
+}
