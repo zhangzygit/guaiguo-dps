730fbe095d5f77006c4b34d809c29d0408132ec4
CHUNBO\zhangzhaoyuan <zhangzhaoyuan@chunbo.com>
2018-09-04 18:01:43 +0800

    RSA

---
+++ guaiguo-dps-web/src/main/java/top/guaiguo/springdps/controller/RSAController.java
@@ -0,0 +1,74 @@
+package top.guaiguo.springdps.controller;
+
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import org.apache.commons.codec.binary.Hex;
+
+/**
+ * Created with IntelliJ IDEA Ultimate.
+ *
+ * @Author zhangzhaoyuan
+ * @Description
+ * @Datetime 2018-09-04 16:15
+ */
+public class RSAController {
+
+
+    //原文
+    private static String src = "imooc security rsa";
+
+    public static void main(String[] args) {
+        jdkSignRSA();
+    }
+
+    /**
+     * 私钥签名，公钥认证
+     */
+    public static void jdkSignRSA() {
+        try {
+            //1.初始化密钥
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(512);
+            KeyPair keyPair = keyPairGenerator.generateKeyPair();
+            RSAPublicKey rsaPublicKey = (RSAPublicKey)keyPair.getPublic();
+            RSAPrivateKey rsaPrivateKey = (RSAPrivateKey)keyPair.getPrivate();
+
+            System.out.println("pub Key:"+Hex.encodeHexString(rsaPublicKey.getEncoded()));
+            System.out.println("pri Key:"+Hex.encodeHexString(rsaPrivateKey.getEncoded()));
+
+            //2.执行签名
+            PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(rsaPrivateKey.getEncoded());
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            PrivateKey privateKey = keyFactory.generatePrivate(pkcs8EncodedKeySpec);
+            //将原文MD5->hash1 将hash1利用私钥生成签名sign
+            Signature signature = Signature.getInstance("MD5withRSA");
+            signature.initSign(privateKey);
+            signature.update(src.getBytes());
+            byte[] result = signature.sign();
+
+            System.out.println("rsa sign : " + Hex.encodeHexString(result));
+
+            //3.验证签名
+            X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(rsaPublicKey.getEncoded());
+            keyFactory = KeyFactory.getInstance("RSA");
+            PublicKey publicKey = keyFactory.generatePublic(x509EncodedKeySpec);
+            //将原文MD5->hash1 将sign利用公钥解密->hash2  验证hash1 == hash2
+            signature = Signature.getInstance("MD5withRSA");
+            signature.initVerify(publicKey);
+            signature.update(src.getBytes());
+            boolean bool = signature.verify(result);
+            System.out.println("jdk rsa verify : " + bool);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
