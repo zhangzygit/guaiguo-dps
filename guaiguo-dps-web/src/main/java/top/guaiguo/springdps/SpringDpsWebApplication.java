c438fde08ace597abcb195d73357e17f83197873
CHUNBO\zhangzhaoyuan <zhangzhaoyuan@chunbo.com>
2019-03-11 18:46:49 +0800

    @EnableEurekaClient

--- guaiguo-dps-web/src/main/java/top/guaiguo/springdps/SpringDpsWebApplication.java
+++ guaiguo-dps-web/src/main/java/top/guaiguo/springdps/SpringDpsWebApplication.java
@@ -2,8 +2,10 @@ package top.guaiguo.springdps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringDpsWebApplication {
 
 	public static void main(String[] args) {
