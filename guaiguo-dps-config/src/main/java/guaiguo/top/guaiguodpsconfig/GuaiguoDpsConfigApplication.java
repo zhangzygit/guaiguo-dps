cd763d0457e2072fa32ad3ef7eab3cdb759127bc
CHUNBO\zhangzhaoyuan <zhangzhaoyuan@chunbo.com>
2019-03-27 18:28:18 +0800

    Hystrix

--- guaiguo-dps-config/src/main/java/guaiguo/top/guaiguodpsconfig/GuaiguoDpsConfigApplication.java
+++ guaiguo-dps-config/src/main/java/guaiguo/top/guaiguodpsconfig/GuaiguoDpsConfigApplication.java
@@ -5,11 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
 @EnableEurekaClient
+@EnableHystrix
+@EnableZuulProxy
 public class GuaiguoDpsConfigApplication {
 
     public static void main(String[] args) {
---
+++ guaiguo-dps-hystrix/src/main/java/top/guaiguo/guaiguodpshystrix/GuaiguoDpsHystrixApplication.java
@@ -0,0 +1,23 @@
+package top.guaiguo.guaiguodpshystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@Controller
+@EnableHystrixDashboard
+public class GuaiguoDpsHystrixApplication {
+
+    @RequestMapping("/")
+    public String home() {
+        return "forward:/hystrix";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuaiguoDpsHystrixApplication.class, args);
+    }
+
+}
